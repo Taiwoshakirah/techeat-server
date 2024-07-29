7f9b8bd5d8a9ccd043627ad4118abe23038bc640
Taiwoshakirah <shakirattaiwo6@gmail.com>
2024-07-29 08:59:07 -0700

    return only cartitem

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,8 +1,6 @@
 const nodemailer = require("nodemailer");
 
 const sendEmail = async (options) => {
-
-  // create reusable transporter object using default SMTP transport
   const transporter = nodemailer.createTransport({
       host: process.env.EMAIL_HOST,
       port: process.env.EMAIL_PORT,
